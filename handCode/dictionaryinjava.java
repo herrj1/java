e07870a20cfece79d2ae45d1d2ced0a66fee98cf
Jonathan <38092600+herrj1@users.noreply.github.com>
2018-08-17 21:35:32 -0400

    Update dictionaryinjava.java

--- handCode/dictionaryinjava.java
+++ handCode/dictionaryinjava.java
@@ -4,10 +4,10 @@ class dictionaryinjava{
 public void main (String [] args){
     Map<String, Label> map = new HashMap<String, Label>();
     for ( String key : map.keySet() ) {
-       //for single keys only
+       //for single key only
     }
     for ( Label value : map.values() ) {
-       //for single values
+       //for single value
     }
     for ( Map.Entry<String, Label> entry : map.entrySet() ) {
         //for keys and values
