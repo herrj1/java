f2e153c8ddbeafbf000a472108fd269d37751121
Jonathan <38092600+herrj1@users.noreply.github.com>
2020-02-29 23:21:53 -0500

    Update io.java

--- handCode/io.java
+++ handCode/io.java
@@ -5,7 +5,7 @@ import java.io.StreamTokenizer;
 
 class io{
 	LinkedList<Integer> numList;
-	/**first */
+	/**first nested method */
 	public io(LinkedList<Integer> numbers){
 		numList = numbers; 
 	}
