3f83917fe1172a786efc7df683ba4f6274f6104d
Jonathan <38092600+herrj1@users.noreply.github.com>
2020-11-15 22:42:51 -0500

    Added new

---
+++ cert/1_working_with_data_types.java
@@ -0,0 +1,11 @@
+
+public 1_use_primitives_and_wrapper_classes{
+	public static main void(String [] args){
+		Integer num = 5;
+		Double price = 1.99;
+		Character initials = 'AJ';
+		System.out.println(num);
+		System.out.println(price);
+		System.out.println(initials);
+	}
+}
