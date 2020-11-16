eb323e131dc33d523a7a90cf845a989a769f2ab5
Jonathan <38092600+herrj1@users.noreply.github.com>
2020-11-15 22:42:27 -0500

    Added new

---
+++ cert/1_use_primitives_and_wrapper_classes.java
@@ -0,0 +1,29 @@
+// Wrapper classes
+// Byte
+// Short
+// Integer
+// Long
+// Float
+// Double
+// Boolean
+// Character
+
+public 1_use_primitives_and_wrapper_classes{
+	public static main void(String [] args){
+		Integer num = 5;
+		Double price = 1.99;
+		Character initials = 'AJ';
+		Byte byte1 = 5;
+		Short short1 = 45;
+		Long long1 = 8554;
+		Boolean bool = true;
+		
+		System.out.println(byte1);
+		System.out.println(long1);
+		System.out.println(num);
+		System.out.println(long1);
+		System.out.println(price);
+		System.out.println(bool);
+		System.out.println(initials);
+	}
+}
