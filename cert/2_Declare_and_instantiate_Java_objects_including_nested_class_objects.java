730b07e21f8322070e7fb4b5a3ac4c26e46761e4
Jonathan <38092600+herrj1@users.noreply.github.com>
2020-11-17 23:42:33 -0500

    Added new

---
+++ cert/2_Declare_and_instantiate_Java_objects_including_nested_class_objects.java
@@ -0,0 +1,11 @@
+
+public 2_Declare_and_instantiate_Java_objects_including_nested_class_objects{
+	public static main void(String [] args){
+		Integer num = 5;
+		Double price = 1.99;
+		Character initials = 'AJ';
+		System.out.println(num);
+		System.out.println(price);
+		System.out.println(initials);
+	}
+}
