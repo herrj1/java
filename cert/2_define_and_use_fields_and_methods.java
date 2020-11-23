4566be16ff66d3e6faa9aabb40ce6c002f7c086d
Jonathan <38092600+herrj1@users.noreply.github.com>
2020-11-22 23:31:24 -0500

    added new

---
+++ cert/2_define_and_use_fields_and_methods.java
@@ -0,0 +1,20 @@
+import java.io.*;
+
+class subject{
+  String name = "";
+
+  public void person(string name){
+    this.name = name;
+  }
+}
+
+
+class 2_define_and_use_fields_and_methods {
+   public static void main(String[] args){
+      Subject p1 = new Subject();
+
+      p1.name("Josh Doe");
+      System.out.println(p1.name);
+   }
+
+}
