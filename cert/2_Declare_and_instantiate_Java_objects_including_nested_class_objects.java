0df5699b974c9744ec18110a386744c6bd20c5ee
Jonathan <38092600+herrj1@users.noreply.github.com>
2020-11-26 21:31:51 -0500

    Update 2_Declare_and_instantiate_Java_objects_including_nested_class_objects.java

--- cert/2_Declare_and_instantiate_Java_objects_including_nested_class_objects.java
+++ cert/2_Declare_and_instantiate_Java_objects_including_nested_class_objects.java
@@ -8,7 +8,7 @@ public class 2_Declare_and_instantiate_Java_objects_including_nested_class_objec
 	}
 	
 	public static main void(String [] args){
-		Car lambo = new Car("New lambo");
+		Car lambo = new Car("New lamborgini");
 		lambo = null; // garbage collection. Part of object life
 	}
 }
