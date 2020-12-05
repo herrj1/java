1802a2aeb5cc48263dad44179470a64851a84a18
Jonathan <38092600+herrj1@users.noreply.github.com>
2020-12-04 22:54:34 -0500

    Added new

---
+++ cert/2_Initialize_objects_and_their_members.java
@@ -0,0 +1,21 @@
+class 2_Initialize_objects_and_their_members{	
+	
+	String blah = "ABC";
+	
+	public 2_Initialize_objects_and_their_members(){
+		System.out.println("Public Constructor");
+	}
+	
+	static {
+		System.out.println("Static Initializer");
+	}
+	
+	{
+		System.out.println("Instance Initializer");
+	}
+	
+	public void static void main(String[] args){
+		new 2_Initialize_objects_and_their_members();
+		new 2_Initialize_objects_and_their_members();
+	}
+}
