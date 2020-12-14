4b3ef00efb2e24e796d751c12addea53ae3d6590
Jonathan <38092600+herrj1@users.noreply.github.com>
2020-12-13 23:28:03 -0500

    Update 2_Declare_and_instantiate_Java_objects_including_nested_class_objects.java

--- cert/2_Declare_and_instantiate_Java_objects_including_nested_class_objects.java
+++ cert/2_Declare_and_instantiate_Java_objects_including_nested_class_objects.java
@@ -6,7 +6,6 @@ public class 2_Declare_and_instantiate_Java_objects_including_nested_class_objec
 	public Car(String model){
 		this.model = model;
 	}
-	
 	public static main void(String [] args){
 		Car lambo = new Car("New lamborgini");
 		lambo = null; // garbage collection. Part of object life
