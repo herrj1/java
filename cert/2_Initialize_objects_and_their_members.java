0c8165b090f8790ca565b2fc3dae8c796156d61f
Jonathan <38092600+herrj1@users.noreply.github.com>
2021-01-09 23:33:49 -0500

    Update 2_Initialize_objects_and_their_members.java

--- cert/2_Initialize_objects_and_their_members.java
+++ cert/2_Initialize_objects_and_their_members.java
@@ -1,6 +1,6 @@
 class 2_Initialize_objects_and_their_members{	
 	
-	String blah = "ABCD";
+	String blah = "ABCDe";
 	
 	public 2_Initialize_objects_and_their_members(){
 		System.out.println("Public Constructor");
